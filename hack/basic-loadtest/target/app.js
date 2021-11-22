4e942be1619e1a6efe4c501d2acbf0425f7053bd
Ezo Saleh <ezo@the-experimenters.com>
2021-11-22 17:19:02 +0000

    Can run a loadtest to completion.

--- hack/basic-loadtest/target/app.js
+++ hack/basic-loadtest/target/app.js
@@ -5,17 +5,20 @@ const port = 3000;
 app.use(express.json());
 
 app.get("/common", (_, res) => {
-  res.send({ route: "/common" });
+    console.log("/common ... hit");
+    res.send({route: "/common"});
 });
 
 app.get("/average", (_, res) => {
-  res.send({ route: "/average" });
+    console.log("/average ... hit");
+    res.send({route: "/average"});
 });
 
 app.get("/rare", (_, res) => {
-  res.send({ route: "/rare" });
+    console.log("/rare ... hit");
+    res.send({route: "/rare"});
 });
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+    console.log(`App listening at http://localhost:${port}`);
 });
