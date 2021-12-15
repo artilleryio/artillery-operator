33c4e377563f3af7cee3db47a1253983026c410c
Ezo Saleh <ezo@the-experimenters.com>
2021-12-15 15:13:05 +0000

    Add RBAC to enable publishing of events.

--- controllers/loadtest_controller.go
+++ controllers/loadtest_controller.go
@@ -44,6 +44,7 @@ type LoadTestReconciler struct {
 // +kubebuilder:rbac:groups=batch,resources=jobs,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=batch,resources=jobs/status,verbs=get
 // +kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch;
+// +kubebuilder:rbac:groups="",resources=events,verbs=create;patch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
