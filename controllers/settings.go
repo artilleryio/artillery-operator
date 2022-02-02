29cb7f5b7178602c33e4666a0091ce1eb3a5d295
Ezo Saleh <ezo@the-experimenters.com>
2022-02-02 15:39:59 +0000

    Added a central location for settings.

---
+++ controllers/settings.go
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2022.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0.
+ *
+ * If a copy of the MPL was not distributed with
+ * this file, You can obtain one at
+ *
+ *   http://mozilla.org/MPL/2.0/
+ */
+
+package controllers
+
+const (
+	AppName = "artillery-operator"
+
+	Version = "version"
+
+	WorkerImage = "ghcr.io/artilleryio/artillery-metrics-enabled:experimental"
+
+	TestScriptVol = "test-script"
+
+	PosthogToken = "_uzX-_WJoVmE_tsLvu0OFD2tpd0HGz72D5sU1zM2hbs"
+)
