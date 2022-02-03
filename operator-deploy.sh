402f33e86f3f2f5d3ca3bc5eb16333c12da22532
Ezo Saleh <ezo@the-experimenters.com>
2022-02-03 12:44:03 +0000

    Updated operator-deploy script to highlight our use of telemetry and how to disable it.

--- operator-deploy.sh
+++ operator-deploy.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+#
+# Copyright (c) 2022.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0.
+#
+# If a copy of the MPL was not distributed with
+# this file, You can obtain one at
+#
+#   http://mozilla.org/MPL/2.0/
+#
+
 set -o errexit
 
 source ./scripts/deploy-helpers.sh
@@ -8,3 +20,10 @@ ensure_kubeconfig
 
 echo ">> Deploying the Artillery LoadTest operator"
 kubectl apply -k config/default
+echo ""
+echo ">> Telemetry is enabled."
+echo ">> We use that telemetry information to help us understand how Artillery is used."
+echo ">> To help us prioritize work on new features and bug fixes, and to help us improve Artillery's performance and stability."
+echo ""
+echo ">> Run the command below if you choose to disable telemetry:"
+echo ">> kubectl -n artillery-operator-system set env deployments/artillery-operator-controller-manager ARTILLERY_DISABLE_TELEMETRY=true"
