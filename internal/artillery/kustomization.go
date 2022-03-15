9e398466e5a86502e2311cce17bad416f3151b7c
Ezo Saleh <ezo@the-experimenters.com>
2022-03-15 12:54:23 +0000

    Inlined kustomization constructor helpers.

--- internal/artillery/kustomization.go
+++ internal/artillery/kustomization.go
@@ -26,49 +26,36 @@ type Kustomization struct {
 }
 
 func NewKustomization(configMapName string, testScriptPath string, labelPrefix string) *Kustomization {
-	k := &Kustomization{
-		Kustomization: &types.Kustomization{},
-	}
-
 	if !filepath.IsAbs(testScriptPath) {
 		testScriptPath = path.Join("..", testScriptPath)
 	}
 
-	k.WithTypeMeta(types.TypeMeta{
-		Kind:       "Kustomization",
-		APIVersion: "kustomize.config.k8s.io/v1beta1",
-	}).WithConfigMapGenerator([]types.ConfigMapArgs{
-		{
-			GeneratorArgs: types.GeneratorArgs{
-				Name: configMapName,
-				KvPairSources: types.KvPairSources{
-					FileSources: []string{testScriptPath},
+	k := &Kustomization{
+		Kustomization: &types.Kustomization{
+			TypeMeta: types.TypeMeta{
+				Kind:       "Kustomization",
+				APIVersion: "kustomize.config.k8s.io/v1beta1",
+			},
+			ConfigMapGenerator: []types.ConfigMapArgs{
+				{
+					GeneratorArgs: types.GeneratorArgs{
+						Name: configMapName,
+						KvPairSources: types.KvPairSources{
+							FileSources: []string{testScriptPath},
+						},
+					},
+				},
+			},
+			GeneratorOptions: &types.GeneratorOptions{
+				DisableNameSuffixHash: true,
+				Labels: map[string]string{
+					"artillery.io/component": fmt.Sprintf("%s-config", labelPrefix),
+					"artillery.io/part-of":   labelPrefix,
 				},
 			},
 		},
-	}).WithGeneratorOptions(&types.GeneratorOptions{
-		DisableNameSuffixHash: true,
-		Labels: map[string]string{
-			"artillery.io/component": fmt.Sprintf("%s-config", labelPrefix),
-			"artillery.io/part-of":   labelPrefix,
-		},
-	})
+	}
 
-	return k
-}
-
-func (k *Kustomization) WithTypeMeta(meta types.TypeMeta) *Kustomization {
-	k.TypeMeta = meta
-	return k
-}
-
-func (k *Kustomization) WithConfigMapGenerator(c []types.ConfigMapArgs) *Kustomization {
-	k.ConfigMapGenerator = c
-	return k
-}
-
-func (k *Kustomization) WithGeneratorOptions(c *types.GeneratorOptions) *Kustomization {
-	k.GeneratorOptions = c
 	return k
 }
 
